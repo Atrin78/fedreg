e7e80ac5569856b1aab52af61fc43eca209d0420
atrin78@student.ubc.ca <atrin78@student.ubc.ca>
2023-08-26 14:44:53 -0700

    dfsdf

--- tasks/mnist/FedAvg_e40_lr1/config.py
+++ tasks/mnist/FedAvg_e40_lr1/config.py
@@ -1,7 +1,14 @@
-from FedUtils.models.mnist.cnn import Model
+from FedUtils.models.mnist.cnn2 import Model
 import torch
 from functools import partial
 from FedUtils.fed.fedavg import FedAvg
+
+transform_fun = transforms.Compose(
+            [
+             torchvision.transforms.Resize((32, 32)),
+             transforms.ToTensor(),
+             transforms.Lambda(lambda x: torch.stack([torch.unsqueeze(x, -1),torch.unsqueeze(x, -1),torch.unsqueeze(x, -1)],2))
+             ])
 
 config = {
 
@@ -22,8 +29,8 @@ config = {
     "use_fed": 1,
     "log_path": "tasks/mnist/FedAvg_e40_lr1/train.log",
 
-    "train_transform": None,
-    "test_transform": None,
+    "train_transform": transform_fun,
+    "test_transform": transform_fun,
     "eval_train": True,
 
 
