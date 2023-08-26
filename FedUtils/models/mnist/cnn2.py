94513b33f508769d15978b722f6f8d2779b1fc09
atrin78@student.ubc.ca <atrin78@student.ubc.ca>
2023-08-26 15:45:25 -0700

    dfsdf

--- FedUtils/models/mnist/cnn2.py
+++ FedUtils/models/mnist/cnn2.py
@@ -106,10 +106,12 @@ class Model(nn.Module):
             func = self.train_onestep
         else:
             func = step_func(self, data)
-        for x, y in data[0]:
-            c = func([x, y])
-            comp += c
-            steps += 1.0
+
+        for _ in range(num_epochs):
+            for x, y in data:
+                c = func([x, y])
+                comp += c
+                steps += 1.0
         soln = self.get_param()
         return soln, comp, weight
 
