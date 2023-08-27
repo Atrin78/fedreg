c746ce2cb674d49feb0d336cdb157d288e9a85dc
atrin78@student.ubc.ca <atrin78@student.ubc.ca>
2023-08-27 00:13:28 -0700

    dfsdf

--- FedUtils/models/mnist/cnn2.py
+++ FedUtils/models/mnist/cnn2.py
@@ -110,6 +110,8 @@ class Model(nn.Module):
         for _ in range(num_epochs):
             train_iters = []
             train_w = [2.0, 1.0]
+            if len(data)==1:
+                train_w = [1.0]
             for train_loader in data:
                 train_iters.append(iter(train_loader))
             for step in range(len(train_iters[0])):
