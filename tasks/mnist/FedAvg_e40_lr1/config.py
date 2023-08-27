f189933a78444a521250cb6a48d522e99118cb16
atrin78@student.ubc.ca <atrin78@student.ubc.ca>
2023-08-26 23:31:11 -0700

    dfsdf

--- tasks/mnist/FedAvg_e40_lr1/config.py
+++ tasks/mnist/FedAvg_e40_lr1/config.py
@@ -7,9 +7,10 @@ import torchvision
 
 transform_fun = transforms.Compose(
             [
-             torchvision.transforms.Resize((16, 16)),
+             torchvision.transforms.Resize((32, 32)),
              transforms.ToTensor(),
         #     transforms.Lambda(lambda x: torch.stack([torch.unsqueeze(x, -1),torch.unsqueeze(x, -1),torch.unsqueeze(x, -1), torch.unsqueeze(x, -1),torch.unsqueeze(x, -1),torch.unsqueeze(x, -1)],2))
+             transforms.Lambda(lambda x: torch.stack([torch.unsqueeze(x, -1),torch.unsqueeze(torch.ones_like(x), -1),torch.unsqueeze(torch.ones_like(x), -1)],2))
              ])
 
 config = {
