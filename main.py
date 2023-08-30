aaf4859c1b6228bb85171299b4a70e750b797609
atrin78@student.ubc.ca <atrin78@student.ubc.ca>
2023-08-30 00:57:48 -0700

    Sdf

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from loguru import logger
 from functools import partial
 import os
 torch.backends.cudnn.deterministic = True
-os.environ['CUDA_VISIBLE_DEVICES']='1'
 
 def allocate_memory():
     total, used = os.popen(
