172ffb4155feb7e813928de1f0c2979c54d1a99d
atrin78@student.ubc.ca <atrin78@student.ubc.ca>
2023-09-04 23:54:47 -0700

    Sdf

--- FedUtils/models/mnist/cnn.py
+++ FedUtils/models/mnist/cnn.py
@@ -5,7 +5,7 @@ import sys
 
 def init_weights(m):
     if isinstance(m, nn.Linear):
-        torch.nn.init.xavier_uniform(m.weight)
+        torch.nn.init.normal(m.weight)
         m.bias.data.fill_(0.01)
 
 
