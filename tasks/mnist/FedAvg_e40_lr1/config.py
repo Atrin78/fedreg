2c987a42ac592720a379b493139e9e18c0d6a7d5
atrin78@student.ubc.ca <atrin78@student.ubc.ca>
2023-09-04 21:58:08 -0700

    Sdf

--- tasks/mnist/FedAvg_e40_lr1/config.py
+++ tasks/mnist/FedAvg_e40_lr1/config.py
@@ -10,7 +10,7 @@ transform_fun = transforms.Compose(
              torchvision.transforms.Resize((28, 28)),
              transforms.ToTensor(),
        #      transforms.Lambda(lambda x: torch.stack([torch.unsqueeze(x, -1),torch.unsqueeze(x, -1),torch.unsqueeze(x, -1)],2)/3.0)
-             transforms.Lambda(lambda x: torch.stack([torch.unsqueeze(x, -1),torch.unsqueeze(torch.ziros_like(x), -1),torch.unsqueeze(torch.ziros_like(x), -1)],2))
+             transforms.Lambda(lambda x: torch.stack([torch.unsqueeze(x, -1),torch.unsqueeze(torch.zeros_like(x), -1),torch.unsqueeze(torch.zeros_like(x), -1)],2))
              ])
 
 config = {
