06e6692f819efb85b5aa80e86963fc9f306b7717
atrin78@student.ubc.ca <atrin78@student.ubc.ca>
2023-09-10 23:04:11 -0700

    Sdf

--- FedUtils/models/mnist/cnn.py
+++ FedUtils/models/mnist/cnn.py
@@ -102,7 +102,7 @@ class Model(nn.Module):
     def AE(self, data):
         if data.device != next(self.parameters()).device:
             data = data.to(next(self.parameters()).device)
-        data = data.reshape(-1, 3, 32, 32)
+        data = data.reshape(-1, 1, 32, 32)
         out = self.net(data)
      #   out = self.head(out)
         out = self.decoder(out)
