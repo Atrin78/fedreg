6770999f7f9cf94a64a985f50c86d9585af61310
atrin78@student.ubc.ca <atrin78@student.ubc.ca>
2023-09-10 23:00:04 -0700

    Sdf

--- FedUtils/models/mnist/cnn.py
+++ FedUtils/models/mnist/cnn.py
@@ -21,10 +21,10 @@ class Model(nn.Module):
     def __init__(self, num_classes, optimizer=None, learning_rate=None, seed=1, p_iters=10, ps_eta=0.1, pt_eta=0.001):
         super(Model, self).__init__()
         self.num_classes = num_classes
-        self.num_inp = 1024*3
+        self.num_inp = 1024
         torch.manual_seed(123+seed)
 
-        self.net = nn.Sequential(*[nn.Conv2d(3, 32, 5), nn.ReLU(), nn.Conv2d(32, 32, 5), nn.MaxPool2d(2), nn.ReLU(), nn.Conv2d(32, 64, 5),
+        self.net = nn.Sequential(*[nn.Conv2d(1, 32, 5), nn.ReLU(), nn.Conv2d(32, 32, 5), nn.MaxPool2d(2), nn.ReLU(), nn.Conv2d(32, 64, 5),
                                  nn.MaxPool2d(2), nn.ReLU(), Reshape(), nn.Linear(1024, 256), nn.ReLU()])
         self.head = nn.Linear(256, self.num_classes)
         self.decoder = nn.Sequential(*[nn.Linear(256, 1024), ReverseReshape(), nn.ConvTranspose2d(64, 32, 5, padding=2), nn.Upsample(scale_factor=2), nn.ReLU(), nn.ConvTranspose2d(32, 32, 5, padding=2), nn.Upsample(scale_factor=2), nn.ReLU(), nn.ConvTranspose2d(32, 3, 5, padding=2), nn.Upsample(scale_factor=2), nn.Sigmoid()])
@@ -94,7 +94,7 @@ class Model(nn.Module):
     def forward(self, data):
         if data.device != next(self.parameters()).device:
             data = data.to(next(self.parameters()).device)
-        data = data.reshape(-1, 3, 32, 32)
+        data = data.reshape(-1, 1, 32, 32)
         out = self.net(data)
         return out
 
