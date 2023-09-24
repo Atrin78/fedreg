7b49bc2a94d390eb1c6131ce20146adb0ae8e26b
atrin78@student.ubc.ca <atrin78@student.ubc.ca>
2023-09-24 02:09:45 -0700

    asd

--- FedUtils/models/utils.py
+++ FedUtils/models/utils.py
@@ -35,10 +35,8 @@ class CusDataset(TensorDataset):
     def __getitem__(self, item):
         if self.transform is None:
             ret = torch.tensor(self.data['x'][item])
-            print(ret.shape)
             if CusDataset.x_mean is not None: 
-                print(CusDataset.x_mean.repeat(ret.shape[0], 1).shape)
-                ret = (ret - CusDataset.x_mean.repeat(ret.shape[0], 1)) / CusDataset.x_std.repeat(ret.shape[0], 1)
+                ret = (ret - CusDataset.x_mean) / CusDataset.x_std
                 print(ret.min())
        #     ret = ret.cpu().detach().numpy().reshape((28, 28))
        #     ret = Image.fromarray(ret).convert('L')
