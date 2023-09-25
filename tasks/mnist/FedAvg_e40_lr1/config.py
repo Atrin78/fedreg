d92717a59336d83af34d0bf3a529102d2849c5a3
atrin78@student.ubc.ca <atrin78@student.ubc.ca>
2023-09-25 03:55:30 -0700

    asd

--- tasks/mnist/FedAvg_e40_lr1/config.py
+++ tasks/mnist/FedAvg_e40_lr1/config.py
@@ -18,7 +18,7 @@ transform_fun = transforms.Compose(
 config = {
 
     "seed": 1,
-    "model": partial(Model, learning_rate=0.1),
+    "model": partial(Model, learning_rate=0.01),
     "inner_opt": None,
     "optimizer": FedAvg,
     "model_param": (10,),
