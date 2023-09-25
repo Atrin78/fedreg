ed5df30fdaa43179a62377066419c001ad725a9a
atrin78@student.ubc.ca <atrin78@student.ubc.ca>
2023-09-24 17:24:02 -0700

    asd

--- FedUtils/fed/fedavg.py
+++ FedUtils/fed/fedavg.py
@@ -27,11 +27,12 @@ def step_func4(model, data):
         pred, features = model.forward_decorr(x)
         _, aux_features = model.forward_decorr(aux_x)
         loss1 = model.loss(pred, y).mean()
+       # loss2 = model.decorr.forward(torch.cat((features, aux_features), 0))
         loss2 = model.decorr.forward(features)
         #print('losses')
         #print(loss1)
         #print(loss2)
-        loss=loss1+0.0*loss2
+        loss=loss1+0.1*loss2
         grad = torch.autograd.grad(loss, parameters)
     #    print('g')
     #    print(grad[0][0])
