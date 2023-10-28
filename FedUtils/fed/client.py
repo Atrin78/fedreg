493aea888087318857aeeaf79e1501c3766a74bf
sanaAyrml <ayromlous@gmail.com>
2023-10-28 08:26:45 -0700

    edit

--- FedUtils/fed/client.py
+++ FedUtils/fed/client.py
@@ -20,12 +20,12 @@ class Client(object):
         self.batchsize = batchsize
         if traincusdataset:  # load data use costomer's dataset
             self.train_data = DataLoader(traincusdataset(train_data, transform=train_transform), batch_size=batchsize, shuffle=True, drop_last=drop_last)
-            self.train_data_fortest = DataLoader(evalcusdataset(train_data, transform=test_transform), batch_size=batchsize, shuffle=False,)
+            self.train_data_fortest = DataLoader(evalcusdataset(train_data, transform=test_transform), batch_size=1, shuffle=False,)
             num_workers = 0
             self.eval_data = [DataLoader(evalcusdataset(ed, transform=test_transform), batch_size=100, shuffle=False, num_workers=num_workers) for ed in eval_data]
         else:
             self.train_data = DataLoader(CusDataset(train_data, transform=train_transform), batch_size=batchsize, shuffle=True, drop_last=drop_last)
-            self.train_data_fortest = DataLoader(CusDataset(train_data, transform=test_transform), batch_size=batchsize, shuffle=False)
+            self.train_data_fortest = DataLoader(CusDataset(train_data, transform=test_transform), batch_size=1, shuffle=False)
             self.eval_data = [DataLoader(CusDataset(ed, transform=test_transform), batch_size=100, shuffle=False) for ed in eval_data]
         self.train_iter = iter(self.train_data)
 
