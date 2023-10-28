53d522e14f154955ea967bfc3619770d35cc1183
sanaAyrml <ayromlous@gmail.com>
2023-10-27 22:28:08 -0700

    edit

--- FedUtils/fed/client.py
+++ FedUtils/fed/client.py
@@ -80,7 +80,7 @@ class Client(object):
     
     def get_cka(self, global_model):
 
-        cka = feature_space_linear_cka(self.model.get_representation(self.train_data_fortest),global_model.get_representation(self.train_data_fortest))
+        cka = feature_space_linear_cka(self.model.get_representation(self.train_data_fortest).numpy(),global_model.get_representation(self.train_data_fortest).numpy())
 
         return cka
 
