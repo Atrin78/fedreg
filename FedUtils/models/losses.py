eaf7440fb417e9aa0808e9effcfcb6b3a08a5337
sanaAyrml <ayromlous@gmail.com>
2023-10-29 17:52:02 -0700

    edit fedntd

--- FedUtils/models/losses.py
+++ FedUtils/models/losses.py
@@ -30,10 +30,9 @@ class NTD_Loss(nn.Module):
             targets = nn.functional.one_hot(targets.long(), self.num_classes).float()
         ce_loss = -targets*torch.log(logits+1e-12)
         ce_loss = ce_loss.sum(1)
-        # ntd_loss = self._ntd_loss(logits, dg_logits, targets)
+        ntd_loss = self._ntd_loss(logits, dg_logits, targets)
 
-        # loss = ce_loss + self.beta * ntd_loss
-        loss = ce_loss.mean()
+        loss = ce_loss.mean() + self.beta * ntd_loss
 
 
         return loss
