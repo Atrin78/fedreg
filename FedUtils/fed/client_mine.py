49f510596b1e0d30100f809033268cf67f77196b
sanaAyrml <ayromlous@gmail.com>
2023-10-30 18:35:54 -0700

    Add new data loader

---
+++ FedUtils/fed/client_mine.py
@@ -0,0 +1,88 @@
+from FedUtils.models.utils import CusDataset, feature_space_linear_cka, cka, gram_linear
+from torch.utils.data import DataLoader, TensorDataset
+import torch.multiprocessing
+torch.multiprocessing.set_sharing_strategy('file_system')
+from loguru import logger
+
+
+class Client(object):
+    def __init__(self, id, train_data, eval_data,model):
+        super(Client, self).__init__()
+        self.rotate=False
+        self.model = model
+        self.id = id
+        self.group = group
+        self.train_samplenum = train_data["datasize"] 
+        self.num_train_samples = train_data["datasize"] 
+        self.num_test_samples = [eval_data["test_datasize"]]
+        drop_last = False
+        self.drop_last = drop_last
+        self.gen_data = None
+        self.batchsize = batchsize
+        self.train_data = train_data["train"]
+        self.train_data_fortest = train_data["train_for_test"]
+        self.eval_data = [eval_data["test"]]
+        self.train_iter = iter(self.train_data)
+
+    def set_param(self, state_dict):
+        self.model.set_param(state_dict)
+        return True
+
+    def get_param(self):
+        return self.model.get_param()
+
+    def solve_grad(self):
+        bytes_w = self.model.size
+        grads, comp = self.model.get_gradients(self.train_data)
+        bytes_r = self.model.size
+        return ((self.num_train_samples, grads), (bytes_w, comp, bytes_r))
+
+    def solve_inner(self, num_epochs=1, step_func=None):
+        bytes_w = self.model.size
+
+        if self.rotate:
+            x_rot = None
+            y_rot = None
+            for d in train_dataloader:
+                x, y = d
+                x = torch.reshape(torch.rot90(torch.reshape(x, (-1, 28, 28)), np.random.choice([1, 3, 4]), [1, 2]), (-1, 784))
+                if x_rot is None:
+                    x_rot = x
+                    y_rot = y
+                else:
+                    x_rot = torch.cat((x_rot, x), 0)
+                    y_rot = torch.cat((y_rot, y), 0)
+            train_dataloader = DataLoader(TensorDataset(x_rot, y_rot), batch_size=self.batchsize, shuffle=True, drop_last=self.drop_last)
+                    
+        if self.gen_data is None:
+            data_loaders = [self.train_data]
+        else:
+            gen_dataloader = DataLoader(self.gen_data, batch_size=self.batchsize, shuffle=True, drop_last=self.drop_last)
+            data_loaders = [self.train_data, gen_dataloader]
+        soln, comp, weight = self.model.solve_inner(data_loaders, num_epochs=num_epochs, step_func=step_func)
+        bytes_r = self.model.size
+        return (self.num_train_samples*weight, soln), (bytes_w, comp, bytes_r)
+
+    def test(self):
+        TC = []
+        LS = []
+        for ed in self.eval_data:
+            total_correct, loss = self.model.test(ed)
+            TC.append(total_correct)
+            LS.append(loss)
+        return TC,  self.num_test_samples
+    
+    
+    def get_cka(self, global_model):
+        local_rep = self.model.get_representation(self.train_data_fortest).cpu().numpy()
+        global_rep = global_model.get_representation(self.train_data_fortest).cpu().numpy()
+        if global_rep.shape[0] > 3:
+            cka_value = cka(gram_linear(local_rep),gram_linear(global_rep), debiased=True)
+        else:
+            cka_value = None
+
+        return cka_value
+
+    def train_error_and_loss(self):
+        tot_correct, loss = self.model.test(self.train_data_fortest)
+        return tot_correct, loss, self.train_samplenum
---
+++ tasks_mine/mnist/FedAvg_e10_lr1/config_new_data.py
@@ -0,0 +1,39 @@
+from FedUtils.models.mnist.cnn import Model
+import torch
+from functools import partial
+from FedUtils.fed.fedavg_mine import FedAvg
+
+config = {
+
+    "seed": 1,
+    "model": partial(Model, learning_rate=1e-1),
+    "inner_opt": None,
+    "optimizer": FedAvg,
+    "model_param": (10,),
+    "inp_size": (784,),
+    "train_path": "/scratch/ssd004/scratch/sayromlou/fedreg_data/data/mnist_10000/data/train/",
+    "test_path": ["/scratch/ssd004/scratch/sayromlou/fedreg_data/data/mnist_10000/data/valid/", "/scratch/ssd004/scratch/sayromlou/fedreg_data/data/mnist_10000/data/test/"],
+    "clients_per_round": 10,
+    "num_rounds": 500,
+    "eval_every": 2,
+    "drop_percent": 0.0,
+    "num_epochs": 10,
+    "batch_size": 50,
+    "number_of_clients": 100,
+    "partition_method": "lda",
+    "shard_per_user": 2,
+    "alpha": 0.1,
+    "root": "/scratch/ssd002/datasets/",
+    "dataset_name": "mnist",
+    "use_fed": 1,
+    "log_path": "tasks_mine/mnist/FedAvg_e10_lr1/train.log",
+    "save_path": "/scratch/ssd004/scratch/sayromlou/fedreg_models/mnist/FedAvg_e10_lr1.pt",
+    "load_path": "/scratch/ssd004/scratch/sayromlou/fedreg_models/mnist/FedAvg_e10_lr1_pretrain.pt",
+
+    "train_transform": None,
+    "test_transform": None,
+    "eval_train": True,
+
+
+
+}
