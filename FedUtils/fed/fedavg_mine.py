60f51240fddd702767101e691b62e359a84b41dc
sanaAyrml <ayromlous@gmail.com>
2023-10-30 22:41:07 -0700

    edit

--- FedUtils/fed/fedavg_mine.py
+++ FedUtils/fed/fedavg_mine.py
@@ -95,7 +95,7 @@ class FedAvg(Server):
                     for x in csolns:
                         logger.info("x: {}".format(soln[1][x].device))
                         logger.info("x: {}".format(soln[0].device))
-                        csolns[x].data.add_(soln[1][x]*soln[0])
+                        csolns[x].data.add_(soln[1][x].detach()*soln[0])
                 if r % self.eval_every == 0:
                     pass
                     if c.model.bottleneck != None:
