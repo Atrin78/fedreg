d82e23ff4e9258006e472d2f951dedeb7b0c4017
atrin78@student.ubc.ca <atrin78@student.ubc.ca>
2023-11-06 16:48:32 -0800

    changes added

--- FedUtils/fed/client_mine.py
+++ FedUtils/fed/client_mine.py
@@ -28,8 +28,12 @@ class Client(object):
         self.train_iter = iter(self.train_data)
 
     def set_param(self, state_dict):
-        #st = {x: state_dict[x] for x in state_dict if x.split('.')[0]!='head'}
         self.model.set_param(state_dict)
+        return True
+
+    def set_param_part(self, state_dict):
+        st = {x: state_dict[x] for x in state_dict if x.split('.')[0]!='head'}
+        self.model.set_param(st)
         return True
 
     def get_param(self):
