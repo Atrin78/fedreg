8d9f6ed30229725c793e252b9104c23f5f9b5f13
sanaAyrml <ayromlous@gmail.com>
2023-11-09 00:35:42 -0800

    edit

--- FedUtils/fed/fedimpress_atrin.py
+++ FedUtils/fed/fedimpress_atrin.py
@@ -291,7 +291,7 @@ class FedImpress(Server):
             [
              transforms.ToTensor(),
              torchvision.transforms.Resize(32),
-             transforms.Lambda(lambda x: torch.stack([x,x,x],0)),
+             transforms.Lambda(lambda x: torch.stack([x.squeeze(0),x.squeeze(0),x.squeeze(0)],0)),
              ])
             if r >= warmup:
                 mnist = torchvision.datasets.MNIST(root='./data', train=True,
