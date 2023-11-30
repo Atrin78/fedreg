f7c8f6fede2f4a9fbc016d52115b3aa4407cdef7
atrin78@student.ubc.ca <atrin78@student.ubc.ca>
2023-11-30 09:41:43 -0800

    dsf

--- FedUtils/fed/fedimpress.py
+++ FedUtils/fed/fedimpress.py
@@ -295,7 +295,7 @@ class FedImpress(Server):
             ll = list(np.concatenate((vis_y, gen_y.cpu().detach().numpy())))
             colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
 
-            ll = [colors[inn] for inn in ll]
+            ll = [colors[int(inn)] for inn in ll]
             emb = TSNE(n_components=2, perplexity=5).fit_transform(np.concatenate((vis_x.cpu().detach().numpy(), gen_x.cpu().detach().numpy())))
             x,y = emb.T
             plt.scatter(x,y, ll)
