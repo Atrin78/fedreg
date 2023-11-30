03bb7f203b8ccc30e60a2c34986fb70d3d877194
atrin78@student.ubc.ca <atrin78@student.ubc.ca>
2023-11-30 09:57:14 -0800

    dsf

--- FedUtils/fed/fedimpress.py
+++ FedUtils/fed/fedimpress.py
@@ -301,7 +301,7 @@ class FedImpress(Server):
             print(x)
             print(y)
             mkr = ['o' if inn<len(vis_x) else 's' for inn in range(len(vis_x)+len(gen_x))]
-            plt.scatter(x,y, c=ll, marker=mkr)
+            plt.scatter(x,y, c=ll, marker=*mkr)
             plt.show()
             plt.savefig('emb.png')
             
