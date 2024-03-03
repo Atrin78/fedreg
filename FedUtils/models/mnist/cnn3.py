532aa8448fb500bfcee85ca129ec0093cbf96dc8
atrin.arya@yahoo.com <atrin.arya@yahoo.com>
2024-03-03 03:21:21 -0800

    ZSDF'

--- FedUtils/models/mnist/cnn3.py
+++ FedUtils/models/mnist/cnn3.py
@@ -38,8 +38,10 @@ class Model(nn.Module):
         self.flop = Flops(self, torch.tensor([[0.0 for _ in range(self.num_inp)]]))
         if torch.cuda.device_count() > 0:
    #         self = self.cuda()
-            self.net = self.net.cuda()
-            self.head = self.head.cuda()
+            device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+            self.to(device)
+            #self.net = self.net.cuda()
+            #self.head = self.head.cuda()
 
     def set_param(self, state_dict):
         self.load_state_dict(state_dict)
