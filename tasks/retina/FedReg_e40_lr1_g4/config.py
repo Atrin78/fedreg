34cb806321da058695f9ffef222f59196e9e03d9
atrin.arya@yahoo.com <atrin.arya@yahoo.com>
2024-03-04 02:02:48 -0800

    SDF

--- tasks/retina/FedReg_e40_lr1_g4/config.py
+++ tasks/retina/FedReg_e40_lr1_g4/config.py
@@ -1,4 +1,4 @@
-from FedUtils.models.mnist.cnn2 import Model
+from FedUtils.models.mnist.cnn3 import Model
 from functools import partial
 from FedUtils.fed.fedreg import FedReg
 from torch.optim import SGD
@@ -20,21 +20,22 @@ config = {
     "model": partial(Model, learning_rate=1e-1, p_iters=10, ps_eta=2e-1, pt_eta=2e-3),
     "inner_opt": None,
     "optimizer": FedReg,
-    "model_param": (10,),
-    "inp_size": (784,),
-    "train_path": "data/mnist_10000/data/train/",
-    "test_path": ["data/mnist_10000/data/valid/", "data/mnist_10000/data/test/"],
-    "clients_per_round": 10,
-    "num_rounds": 500,
+    "model_param": (2,),
+    "inp_size": (128*128*3,),
+    "train_path": "retina",
+    "test_path": ["retina"],
+    "clients_per_round": 3,
+    "num_rounds": 40,
     "eval_every": 1,
     "drop_percent": 0.0,
-    "num_epochs": 40,
-    "batch_size": 10,
+    "num_epochs": 10,
+    "batch_size": 40,
     "use_fed": 1,
-    "log_path": "tasks/mnist/FedReg_e40_lr1_g4/train.log",
+    "log_path": "tasks/retina/FedReg_e40_lr1_g4/train.log",
     "train_transform": None,
     "test_transform": None,
     "eval_train": True,
+    "data_size":100,
     "gamma": 0.4,
 
 }
