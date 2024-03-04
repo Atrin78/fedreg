7af31a2d0560a9284848188c4603b98ad03dea1f
atrin.arya@yahoo.com <atrin.arya@yahoo.com>
2024-03-04 02:40:42 -0800

    SDF

--- tasks/retina/FedCurv_e40_lr1_g4/config.py
+++ tasks/retina/FedCurv_e40_lr1_g4/config.py
@@ -1,4 +1,4 @@
-from FedUtils.models.mnist.cnn import Model
+from FedUtils.models.mnist.cnn3 import Model
 import torch
 from functools import partial
 from FedUtils.fed.fedcurv import FedCurv
@@ -6,21 +6,22 @@ from FedUtils.fed.fedcurv import FedCurv
 config = {
 
     "seed": 1,
-    "model": partial(Model, learning_rate=1e-1),
+    "model": partial(Model, learning_rate=1e-2),
     "inner_opt": None,
     "optimizer": FedCurv,
-    "model_param": (10,),
-    "inp_size": (784,),
-    "train_path": "data/mnist_10000/data/train/",
-    "test_path": ["data/mnist_10000/data/valid/", "data/mnist_10000/data/test/"],
-    "clients_per_round": 10,
-    "num_rounds": 500,
+    "model_param": (2,),
+    "inp_size": (128*128*3,),
+    "train_path": "retina",
+    "test_path": ["retina"],
+    "clients_per_round": 3,
+    "num_rounds": 40,
     "eval_every": 1,
     "drop_percent": 0.0,
-    "num_epochs": 40,
-    "batch_size": 10,
+    "num_epochs": 10,
+    "batch_size": 40,
     "use_fed": 1,
-    "log_path": "tasks/mnist/FedCurv_e40_lr1_g4/train.log",
+    "log_path": "tasks/retina/FedCurv_e40_lr1_g4/train.log",
+    "data_size":100,
     "gamma": 1e-4,
 
     "train_transform": None,
