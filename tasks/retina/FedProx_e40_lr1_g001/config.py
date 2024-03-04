d0349ec925a64b27d3584ef5081a9717b557591f
atrin.arya@yahoo.com <atrin.arya@yahoo.com>
2024-03-03 21:18:56 -0800

    SDF

--- tasks/retina/FedProx_e40_lr1_g001/config.py
+++ tasks/retina/FedProx_e40_lr1_g001/config.py
@@ -1,4 +1,4 @@
-from FedUtils.models.mnist.cnn import Model
+from FedUtils.models.mnist.cnn3 import Model
 import torch
 from functools import partial
 from FedUtils.fed.fedprox import FedProx
@@ -9,18 +9,19 @@ config = {
     "model": partial(Model, learning_rate=1e-1),
     "inner_opt": None,
     "optimizer": FedProx,
-    "model_param": (10,),
-    "inp_size": (784,),
-    "train_path": "data/mnist_10000/data/train/",
-    "test_path": ["data/mnist_10000/data/valid/", "data/mnist_10000/data/test/"],
-    "clients_per_round": 10,
-    "num_rounds": 500,
+    "model_param": (2,),
+    "inp_size": (128*128*3,),
+    "train_path": "retina",
+    "test_path": ["retina"],
+    "clients_per_round": 4,
+    "num_rounds": 40,
     "eval_every": 1,
     "drop_percent": 0.0,
-    "num_epochs": 40,
-    "batch_size": 10,
+    "num_epochs": 10,
+    "batch_size": 60,
     "use_fed": 1,
-    "log_path": "tasks/mnist/FedProx_e40_lr1_g001/train.log",
+    "data_size":400,
+    "log_path": "tasks/retina/FedProx_e40_lr1_g001/train.log",
     "gamma": 0.001,
 
     "train_transform": None,
