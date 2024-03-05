a42a01e880bf8e85db4ce2edbb1b02e69f3c2164
atrin.arya@yahoo.com <atrin.arya@yahoo.com>
2024-03-04 23:43:05 -0800

    XDGF

--- FedUtils/models/medmnist_dataset.py
+++ FedUtils/models/medmnist_dataset.py
@@ -21,7 +21,7 @@ def prepare_bloodmnist(datasets, im_size):
     test_data = {}
     for i in range(8):
         data_train = Modified_medmnist(split='train', chunk=i, image_size=128)
-        data_test = Modified_medmnist(split='test', chunk=i, image_size=128)
+        data_test = Modified_medmnist(split='test', chunk=i, image_size=128, mean=data_train.mean, std=data_train.std)
         x, y = next(iter(DataLoader(data_train, batch_size=len(data_train))))
         train_data[str(i)] = {'x':x.cpu().detach().numpy(), 'y':y.cpu().detach().numpy()}
         x, y = next(iter(DataLoader(data_test, batch_size=len(data_test))))
